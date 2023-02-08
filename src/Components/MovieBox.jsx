4ceeabd25b3857696def9f415f00321fe7fc8eb6
ahmetcetnn <ahmetlaw@outlook.com>
2023-02-08 23:05:20 +0300

    few changes

--- src/Components/MovieBox.jsx
+++ src/Components/MovieBox.jsx
@@ -25,10 +25,10 @@ const MovieBox =({title, poster_path, vote_average, release_date, overview})=>{
                       <Modal.Body>
                       <img className="card-img-top" style={{width:'14rem'}}src={API_IMG+poster_path} />
                       <h3>{title}</h3>
-                      <h4>IMsfew changesDb: {vote_average}</h4>
-                      <h5>Vizyodn Tsarihsi: {release_date}</h5>
+                      <h4>IMDb: {vote_average}</h4>
+                      <h5>Vizyon Tarihi: {release_date}</h5>
                       <br></br>
-                      <h6>Gensfew changesel Bakış</h6>
+                      <h6>Genel Bakış</h6>
                       <p>{overview}</p>
                       </Modal.Body>
                       <Modal.Footer>
