72f017afc26c4eb5457622dbf900404889bcd587
ahmetcetnn <ahmetlaw@outlook.com>
2023-02-11 21:59:46 +0300

    few changes

--- src/Components/MovieBox.jsx
+++ src/Components/MovieBox.jsx
@@ -26,7 +26,7 @@ const MovieBox =({title, poster_path, vote_average, release_date, overview})=>{
                       <img className="card-img-top" style={{width:'14rem'}}src={API_IMG+poster_path} />
                       <h3>{title}</h3>
                       <h4>IMDb: {vote_average}</h4>
-                      <h5>Vizyon  Tarihi: {release_date}</h5>
+                      <h5>Vizyon Tarihi: {release_date}</h5>
                       <br></br>
                       <h6>Genel Bakış</h6>
                       <p>{overview}</p>
